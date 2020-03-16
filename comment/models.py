9cb6e9ab5daf4dadf58bb9aaadeabffc425ffdb3
crazy_heng <778138382@qq.com>
2020-03-16 16:25:13 +0800

    完成了js四个小功能

--- comment/models.py
+++ comment/models.py
@@ -11,7 +11,7 @@ class Comment(models.Model):
     """
     评论表
     """
-    article = models.ForeignKey(to=ArticlePost, blank=True, null=True, verbose_name='评论属于哪篇博客', on_delete=models.CASCADE)
+    article = models.ForeignKey(to=ArticlePost, blank=True, null=True, verbose_name='评论属于哪篇博客', on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(to=User, blank=True, null=True, verbose_name='该评论是谁写的', on_delete=models.CASCADE)
     # body = models.TextField(verbose_name='评论内容')
     body = RichTextField(verbose_name='评论内容')  # 评论内容使用富文本插件的字段
